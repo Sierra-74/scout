abe51dbac8767caa4ba67e65293eb9cc7087d4fb
Sierra-74 <marcasleahy@gmail.com>
2026-05-28 12:52:46 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,38 @@
+const CACHE_NAME = 'scout-tool-v1';
+
 self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open('scout-tool-v1').then(function(cache) {
-      return cache.addAll([
-        './',
-        './index.html'
-      ]);
-    })
-  );
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            return cache.addAll([
+                './',
+                './index.html',
+                './manifest.json'
+            ]);
+        })
+    );
+    self.skipWaiting(); // ✅ activate new version immediately
+});
+
+self.addEventListener('activate', function(e) {
+    e.waitUntil(
+        caches.keys().then(function(keys) {
+            return Promise.all(
+                keys.filter(k => k !== CACHE_NAME)
+                    .map(k => caches.delete(k))
+            );
+        })
+    );
+    self.clients.claim();
 });
 
 self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
+    e.respondWith(
+        fetch(e.request)
+            .then(res => {
+                let copy = res.clone();
+                caches.open(CACHE_NAME).then(cache => cache.put(e.request, copy));
+                return res;
+            })
+            .catch(() => caches.match(e.request))
+    );
 });
-``
